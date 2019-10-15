4b4015c330dd1a73f8e06095a48fc27986f12023
kid <kid.1447250889@live.com>
2019-10-16 00:37:20 +0800

    时间戳问题
    还差 删除, 固化前端学号...

--- springBoot/src/main/java/io/kid19999/backstage/controller/userShow.java
+++ springBoot/src/main/java/io/kid19999/backstage/controller/userShow.java
@@ -8,14 +8,10 @@ import io.kid19999.backstage.mapper.adminMapper;
 import io.kid19999.backstage.mapper.userMapper;
 import io.kid19999.backstage.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.Date;
-import java.util.List;
 
 @RestController
 public class userShow {
@@ -51,6 +47,24 @@ public class userShow {
     QueryWrapper<User> wrapper = new QueryWrapper<>();
     Page<User> page =new Page<>(pageNo,pageSize);
     return userMapper.selectPage(page,wrapper);
+  }
+
+
+
+  @PatchMapping("/userUpdate")
+  Result update(@RequestBody User user){
+    System.out.println(user);
+
+    // 时间戳问题
+    Date d = new Date();
+    Timestamp date = new Timestamp(d.getTime());
+    user.setUserCreateTime(date);
+    User u = userMapper.selectById(user.getUserId());
+    Result res = new Result();
+    res.setReslut(1);
+    res.setInfo("更新成功!");
+    userMapper.updateById(user);
+    return res;
   }
 
 }
--- vue-ui/src/views/Users.vue
+++ vue-ui/src/views/Users.vue
@@ -5,37 +5,39 @@
 
     <!--  列表  -->
     <a-table bordered :dataSource="dataSource" :columns="columns">
-    <!--   修改   -->
-      <template slot="name" slot-scope="text, record">
-        <editable-cell :text="text" @change="onCellChange(record.key, 'name', $event)"/>
+      <!--   修改   -->
+      <template slot="userName" slot-scope="text, record">
+        <editable-cell :text="text" @change="onCellChange(record.key, 'userName', $event)"/>
       </template>
 
-      <template slot="id" slot-scope="text, record">
-        <editable-cell :text="text" @change="onCellChange(record.key, 'id', $event)"/>
+      <template slot="userId" slot-scope="text, record">
+        <editable-cell :text="text" @change="onCellChange(record.key, 'userId', $event)"/>
       </template>
 
-      <template slot="phone" slot-scope="text, record">
-        <editable-cell :text="text" @change="onCellChange(record.key, 'phone', $event)"/>
+      <template slot="userPhone" slot-scope="text, record">
+        <editable-cell :text="text" @change="onCellChange(record.key, 'userPhone', $event)"/>
       </template>
 
-      <template slot="class" slot-scope="text, record">
-        <editable-cell :text="text" @change="onCellChange(record.key, 'class', $event)"/>
+      <template slot="userClass" slot-scope="text, record">
+        <editable-cell :text="text" @change="onCellChange(record.key, 'userClass', $event)"/>
       </template>
 
-      <template slot="qq" slot-scope="text, record">
-        <editable-cell :text="text" @change="onCellChange(record.key, 'qq', $event)"/>
+      <template slot="userQq" slot-scope="text, record">
+        <editable-cell :text="text" @change="onCellChange(record.key, 'userQq', $event)"/>
       </template>
 
-      <template slot="email" slot-scope="text, record">
-        <editable-cell :text="text" @change="onCellChange(record.key, 'email', $event)"/>
+      <template slot="userEmail" slot-scope="text, record">
+        <editable-cell :text="text" @change="onCellChange(record.key, 'userEmail', $event)"/>
       </template>
 
-    <!--   删除   -->
+      <!--   删除   -->
+
       <template slot="operation" slot-scope="text, record">
         <a-popconfirm
           v-if="dataSource.length"
           title="Sure to delete?"
-          @confirm="() => onDelete(record.key)">
+          @confirm="() => onDelete(record.key)"
+        >
           <a href="javascript:;">Delete</a>
         </a-popconfirm>
       </template>
@@ -44,121 +46,161 @@
   </div>
 </template>
 <script>
-  import axios from 'axios';
-  import EditableCell from '../components/EditableCell';
-  export default {
-    components: {
-      EditableCell,
-    },
-    data () {
-      return {
-        dataSource: [{
-          userId: '01234541345',
-          userName: 'Edward King 0',
-          userClass: '32',
-          userQq: '456125411',
-          userPhone:'12345641',
-          userCreateTime:'2019-10-13T00:00:00.000+0000',
-          userEmail:'dada@qq.com',
-        }],
-        count: 2,
-        columns: [{
-          title: '姓名',
-          dataIndex: 'userName',
-          scopedSlots: { customRender: 'name' },
-        }, {
-          title: '学号',
-          dataIndex: 'userId',
-          scopedSlots: { customRender: 'id' },
-        }, {
-          title: '班级',
-          dataIndex: 'userClass',
-          scopedSlots: { customRender: 'class' },
+    import axios from 'axios';
+    import EditableCell from '../components/EditableCell';
+    export default {
+        components: {
+            EditableCell,
         },
-          {
-            title: '手机号',
-            dataIndex: 'userPhone',
-            scopedSlots: { customRender: 'phone' },
-          },
-          {
-            title: 'QQ',
-            dataIndex: 'userQq',
-            scopedSlots: { customRender: 'qq' },
-          },
-          {
-            title: 'Email',
-            dataIndex: 'userEmail',
-            scopedSlots: { customRender: 'email' },
-          },
-          {
-            title: '创建时间',
-            dataIndex: 'userCreateTime',
-            scopedSlots: { customRender: 'time' },
-          },
-          {
-          title: '操作',
-          dataIndex: 'operation',
-          scopedSlots: { customRender: 'operation' },
-        }],
-      }
-    },
-    methods: {
-      onCellChange (key, dataIndex, value) {
-        const dataSource = [...this.dataSource]
-        const target = dataSource.find(item => item.key === key)
-        console.info(target);
-        console.info(target['userCreateTime'])
-        if (target) {
-          target[dataIndex] = value
-          this.dataSource = dataSource
-        }
-      },
-      //删除
-      onDelete (key) {
-        const dataSource = [...this.dataSource]
-        this.dataSource = dataSource.filter(item => item.key !== key)
-        this.count -= 1;
-      },
-      //增加
-      handleAdd () {
-        const { count, dataSource } = this
-        const newData = {
-          userId: '必填',
-          userName: '必填',
-          userClass: '必填',
-          userQq: '必填',
-          userPhone:'',
-          userCreateTime:'',
-          userEmail:'',
-        }
-        this.dataSource = [...dataSource, newData]
-        this.count = count + 1;
-      },
-      to_update: function(target) {
-        let self = this;
-        axios.post('/userUpdate', {
-          userId:target['id'],
-          userName:target['name'],
-          userEmail:target['email'],
-          userPhone:target['phone'],
-          userQq:target['qq'],
-          userClass:target['class'],
-        }).then(function (response) {
-          let res = response.data;
-          if(res['reslut'] === 1){
-            self.$message({
-              message: res['info'],
-              type: 'success'
-            });
-          }else{
-            self.$message.error(res['info']);
-          }
-        }).catch(function (error) {
-          console.log(error);
-        });
-      },
-    },
-  }
+        data ()  {
+            return {
+                dataSource: [{
+                    key:'123154542123',
+                    userId: '01234541345',
+                    userName: 'Edward King 0',
+                    userClass: '32',
+                    userQq: '456125411',
+                    userPhone:'12345641',
+                    userCreateTime:'2019-10-13T00:00:00.000+0000',
+                    userEmail:'dada@qq.com',
+                }],
+                count: 2,
+                columns: [{
+                    title: '姓名',
+                    dataIndex: 'userName',
+                    scopedSlots: { customRender: 'userName' },
+                }, {
+                    title: '学号',
+                    dataIndex: 'userId',
+                    scopedSlots: { customRender: 'userId' },
+                }, {
+                    title: '班级',
+                    dataIndex: 'userClass',
+                    scopedSlots: { customRender: 'userClass' },
+                },
+                    {
+                        title: '手机号',
+                        dataIndex: 'userPhone',
+                        scopedSlots: { customRender: 'userPhone' },
+                    },
+                    {
+                        title: 'QQ',
+                        dataIndex: 'userQq',
+                        scopedSlots: { customRender: 'userQq' },
+                    },
+                    {
+                        title: 'Email',
+                        dataIndex: 'userEmail',
+                        scopedSlots: { customRender: 'userEmail' },
+                    },
+                    {
+                        title: '创建时间',
+                        dataIndex: 'userCreateTime',
+                        scopedSlots: { customRender: 'userCreateTime' },
+                    },
+                    {
+                        title: '操作',
+                        dataIndex: 'operation',
+                        scopedSlots: { customRender: 'operation' },
+                    }],
+            }
+        },
+        mounted(){
+            this.to_getList(1,5);
+        },
+        methods: {
+            // 通过 target 构建一个userJson
+            to_buildOne(target){
+                var data = {
+                    userId:target.userId,
+                    userName:target.userName,
+                    userEmail:target.userEmail,
+                    userPhone:target.userPhone,
+                    userQq:target.userQq,
+                    userClass:target.userClass,
+            };
+                return data;
+            },
+            // 改变值时
+            onCellChange (key, dataIndex, value) {
+                const dataSource = [...this.dataSource];
+                const target = dataSource.find(item => item.key === key);
+                console.info(target);
+                if (target) {
+                    target[dataIndex] = value;
+                    this.dataSource = dataSource
+                }
+                var data = this.to_buildOne(target);
+                console.info(data);
+                this.to_update(data);
+            },
+            //删除
+            onDelete (key) {
+                const dataSource = [...this.dataSource];
+                this.dataSource = dataSource.filter(item => item.key !== key);
+                this.count -= 1;
+            },
+            //增加
+
+            handleAdd() {
+                const { count, dataSource } = this;
+                const newData = {
+                    key:this.count+1,
+                    userId: '必填',
+                    userName: '必填',
+                    userClass: '必填',
+                    userQq: '必填',
+                    userPhone:'',
+                    userCreateTime:'',
+                    userEmail:'',
+                };
+                this.dataSource = [...dataSource, newData];
+                this.count = count + 1;
+            },
+
+
+            // 更新一个数据
+            to_update(data) {
+                let self = this;
+                axios.patch('/userUpdate', data).then(function (response) {
+                    let res = response.data;
+                    if(res['reslut'] === 1){
+                        self.$message({
+                            message: res['info'],
+                            type: 'success'
+                        });
+                    }else{
+                        self.$message.error(res['info']);
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                });
+            },
+
+            // 初始化 获取全部列表
+            to_getList(pageNo,pageSize){
+                let self = this;
+                axios.get('/userList?'+'pageNo='+pageNo+'&pageSize='+pageSize).then(function(response){
+                    self.dataSource =  response.data.records;
+                    for(let i = 0;i < self.dataSource.length; i++) {
+                        self.dataSource[i]['key'] = self.dataSource[i]['userId'];
+                    }
+                })
+            },
+            // 删除 一行数据
+            to_deleteOne:function(target){
+                let self = this;
+                axios.delete('/userDelete',this.to_buildOne(target)).then(function(response){
+                    self.$message({
+                        message: response.data['info'],
+                        type: 'success'
+                    });
+                })
+            },
+
+
+        },
+    }
 </script>
 <style>
   .editable-cell {
